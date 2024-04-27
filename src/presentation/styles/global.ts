b5ed99e679136c14c47bd8c915305c567c037cd6
lucassec1 <lucasemmanuelcs@gmail.com>
2024-04-27 15:52:49 -0300

    :sparkles: feat: add global styles

---
+++ src/presentation/styles/global.ts
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    font-family: 'GothamMedium', sans-serif;
+    font-size: 14px;
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+`;
