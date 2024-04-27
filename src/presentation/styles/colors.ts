75ca6bba469fba4d380257e342c121ae8d7f2c23
lucassec1 <lucasemmanuelcs@gmail.com>
2024-04-27 15:52:35 -0300

    :sparkles: feat: add colors variable

---
+++ src/presentation/styles/colors.ts
@@ -0,0 +1,14 @@
+export const colors = {
+  $primary: '#333333',
+  $black: '#000000',
+  $white: '#FFFFFF',
+  $gray: '#3A3A3A',
+  $lightGray: '#898989',
+  $lightGray2: '#B0B0B0',
+  $lightGray3: '#9F9F9F',
+  $golden: '#B88E2F',
+  $red: '#E97171',
+  $green: '#2EC1AC',
+  $blue: '#816DFA',
+  $yellow: '#FFC700',
+};
